36bee8883219dc520cd01f7ace8b1419d6fa633c
Scott C. Livingston <scott@rerobots.net>
2024-09-06 17:19:19 -0700

    code style

--- multi_kobuki/ROS1/multi_kobuki_wander/nodes/rev.py
+++ multi_kobuki/ROS1/multi_kobuki_wander/nodes/rev.py
@@ -4,6 +4,7 @@ Copyright (C) 2020 rerobots, Inc.
 This is free software, released under the Apache License, Version 2.0.
 You may obtain a copy of the License at https://www.apache.org/licenses/LICENSE-2.0
 """
+
 import time
 
 import rospy
@@ -17,7 +18,7 @@ def main():
     st = time.time()
     while not rospy.is_shutdown() and time.time() - st < 10:
         m = Twist()
-        m.linear.x = -.1
+        m.linear.x = -0.1
         movec.publish(m)
         rate.sleep()
     m = Twist()
